a8826af9732cf56f8a1859118bc1b05639a6d5a0
Edward Lum <dim.lyrintzis@gmail.com>
2023-07-10 23:34:17 +0300

    Finished chapter 1

--- code/chapter-1.py
+++ code/chapter-1.py
@@ -10,18 +10,17 @@ if not os.path.exists(data_folder):
 if not os.path.exists(output_folder):
   os.mkdir(output_folder)
 
-points = [(0.1, 0.5), (0.5, 0.5), (0.9, 0.5)]
-
-# Zip the points list to x and y to be used in the plot method
-x, y = zip(*points)
+point1 = (4, 1)
+point2 = (3, 4)
 
 # Create a figure and an axis
 fig, ax = plt.subplots(1, 1)
 fig.set_size_inches(5, 5)
-ax.plot(x, y, color='green', marker='o', linestyle='None')
+ax.plot(*point1, color='green', marker='^')
+ax.plot(*point2, color='red', marker='o')
 
 # Save figure as a .png file to the output directory
-output_path = os.path.join(output_folder, 'simple.png')
+output_path = os.path.join(output_folder, 'exercise1.png')
 plt.savefig(output_path)
 
 # Draw the plot
