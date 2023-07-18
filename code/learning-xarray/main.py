bf649643ca68e2ec3fad23fad04c93013e2b0977
Edward Lum <dim.lyrintzis@gmail.com>
2023-07-18 22:57:08 +0300

    Minor change

--- code/learning-xarray/main.py
+++ code/learning-xarray/main.py
@@ -4,4 +4,5 @@ import xarray as xr
 home_dir = os.path.expanduser('~')
 file_path = os.path.join(home_dir, 'Code/star-struck/data/download.grib')
 
-ds = xr.open_dataset(file_path, engine='cfgrib')
+geo_data = xr.open_dataset(file_path, engine='cfgrib')
+
