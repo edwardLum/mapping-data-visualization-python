a730e3ba2a99d383161f79fbdd8f7bf55077163a
Edward Lum <dim.lyrintzis@gmail.com>
2023-07-20 01:33:11 +0300

    Added ex1 to xarray code

--- code/learning-xarray/main.py
+++ code/learning-xarray/main.py
@@ -1,5 +1,6 @@
 import os
 import xarray as xr
+import matplotlib.pyplot as plt
 
 home_dir = os.path.expanduser('~')
 file_path = os.path.join(home_dir, 'Code/star-struck/data/download.grib')
@@ -7,19 +8,29 @@ file_path = os.path.join(home_dir, 'Code/star-struck/data/download.grib')
 ds = xr.open_dataset(file_path, engine='cfgrib')
 
 # Print dimension coordinates of the given dataset
+#
+#print(ds['time'])
+#
+#print(ds['step'])
+#
+#print(ds['latitude'])
+#
+#print(ds['longitude'])
+#
+## Print non dimension coordinates 
+#
+#print(ds['number'])
+#
+#print(ds['surface'])
+#
+#print(ds['valid_time'])
+#
+# Exercise 1.1: Plot time series for a specific location
 
-print(ds['time'])
+# Select Attica 
+lat_min, lat_max = 38.25, 37.70 
+lon_min, lon_max = 23.45, 24.25
+subset = ds.t2m.sel(latitude=slice(lat_min, lat_max), longitude=slice(lon_min, lon_max))
 
-print(ds['step'])
-
-print(ds['latitude'])
-
-print(ds['longitude'])
-
-# Print non dimension coordinates 
-
-print(ds['number'])
-
-print(ds['surface'])
-
-print(ds['valid_time'])
+subset.plot()
+plt.show()
