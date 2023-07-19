14f6123dad1854ba501ffc56552600d162200f43
Edward Lum <dim.lyrintzis@gmail.com>
2023-07-19 23:45:49 +0300

    Defined dimension and non dimension coordinates of the dataset

--- code/learning-xarray/main.py
+++ code/learning-xarray/main.py
@@ -4,5 +4,22 @@ import xarray as xr
 home_dir = os.path.expanduser('~')
 file_path = os.path.join(home_dir, 'Code/star-struck/data/download.grib')
 
-geo_data = xr.open_dataset(file_path, engine='cfgrib')
+ds = xr.open_dataset(file_path, engine='cfgrib')
 
+# Print dimension coordinates of the given dataset
+
+print(ds['time'])
+
+print(ds['step'])
+
+print(ds['latitude'])
+
+print(ds['longitude'])
+
+# Print non dimension coordinates 
+
+print(ds['number'])
+
+print(ds['surface'])
+
+print(ds['valid_time'])
