5cb29df96008ccbef5a4234954905ec955023714
Edward Lum <dim.lyrintzis@gmail.com>
2023-08-09 00:04:54 +0300

    Restructured repo

--- code/ad-hoc/u-series.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-s = pd.Series(np.random.randn(5), index=["a", "b", "c", "d", "e"])
-
-d = {"b": 1, "a": 0, "c": 2}
-
-pd.Series(d, index=["b", "c", "d", "a"])
